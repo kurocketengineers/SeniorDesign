3d0b919b58d134d1f2563ed3e6b536b4ed5c7f6b
Capnewfoundland <capnewfoundland@gmail.com>
2016-02-05 10:16:53 -0600

    Updated App to recognize the simple chat service.

--- App/BluetoothLeGatt/Application/src/main/java/com/example/android/bluetoothlegatt/SampleGattAttributes.java
+++ App/BluetoothLeGatt/Application/src/main/java/com/example/android/bluetoothlegatt/SampleGattAttributes.java
@@ -28,11 +28,12 @@ public class SampleGattAttributes {
 
     static {
         // Sample Services.
-        attributes.put("0000180d-0000-1000-8000-00805f9b34fb", "Heart Rate Service");
-        attributes.put("0000180a-0000-1000-8000-00805f9b34fb", "Device Information Service");
+        attributes.put("713d0000-503e-4c75-ba94-3148f18d941e", "Eddy-Ha Lives!");
+        attributes.put("713d0003-503e-4c75-ba94-3148f18d941e", "TX");
+        attributes.put("713d0002-503e-4c75-ba94-3148f18d941e", "RX");
         // Sample Characteristics.
-        attributes.put(HEART_RATE_MEASUREMENT, "Heart Rate Measurement");
-        attributes.put("00002a29-0000-1000-8000-00805f9b34fb", "Manufacturer Name String");
+        //attributes.put(HEART_RATE_MEASUREMENT, "Heart Rate Measurement");
+        //attributes.put("00002a29-0000-1000-8000-00805f9b34fb", "Manufacturer Name String");
     }
 
     public static String lookup(String uuid, String defaultName) {
